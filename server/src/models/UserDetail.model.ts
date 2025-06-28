ed1f59f48cbe8db007378991baaaa80bcfb83804
diksha1627 <dikshabuxani84745@gmail.com>
2025-06-28 15:59:31 +0400

    user login and signup api created with jwt token and password hashing

--- server/src/models/UserDetail.model.ts
+++ server/src/models/UserDetail.model.ts
@@ -1,14 +1,23 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserDetailSchema = new Schema({
   _id: { type: Schema.Types.ObjectId, auto: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   emailAddress: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, 
   createdOn: { type: Date, default: Date.now },
   createdBy: { type: String },
   modifiedOn: { type: Date },
   modifiedBy: { type: String },
+});
+
+UserDetailSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 export default model('UserDetail', UserDetailSchema);
