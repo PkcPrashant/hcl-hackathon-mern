161a504a2f3cfb6301844cceacc14f0dfa147084
Otabek <45333849+u1810291@users.noreply.github.com>
2025-07-10 11:39:53 +0400

    Update index.ts

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,4 +2,4 @@ import { config } from 'dotenv';
 
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://admin:12345Admin@cluster0.t2qzfpk.mongodb.net/hcl?retryWrites=true&w=majority"
+export const MONGODB_URI = process.env.MONGODB_URI || ""
